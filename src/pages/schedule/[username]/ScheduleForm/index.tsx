45783ef75eeb28dbf72be9dbf355c7aa2b7f42d8
João Gabriel <main.joaogabrielsg@gmail.com>
2023-02-08 19:34:30 -0300

    feat: render calendar based on month

--- src/pages/schedule/[username]/ScheduleForm/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/index.tsx
@@ -1,7 +1,6 @@
 import { CalendarStep } from './CalendarStep'
-import { ConfirmStep } from './ConfirmStep'
+// import { ConfirmStep } from './ConfirmStep'
 
 export function ScheduleForm() {
-  return <ConfirmStep />
-  // return <CalendarStep />
+  return <CalendarStep />
 }
