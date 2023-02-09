83344c164f06efe1891a8a85e198f5039e462100
João Gabriel <main.joaogabrielsg@gmail.com>
2023-02-09 20:15:52 -0300

    chore: configure react-query

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,7 +1,9 @@
 import '../lib/dayjs'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
+import { QueryClientProvider } from '@tanstack/react-query'
 import { globalStyles } from '../styles/global'
+import { queryClient } from '../lib/react-query'
 
 globalStyles()
 
@@ -10,8 +12,10 @@ export default function App({
   pageProps: { session, ...pageProps },
 }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </QueryClientProvider>
   )
 }
